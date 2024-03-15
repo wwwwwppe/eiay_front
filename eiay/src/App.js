7d693314c0eb37730a18041667d12f951dd2ea15
zh137 <yz456546@qq.com>
2024-03-15 17:18:17 +0800

    目录结构调整

--- eiay/src/App.js
+++ eiay/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<frameElement>
+			<div>
+				<h1>React App</h1>
+			</div>
+			<div>
+				<h1>React App too</h1>
+			</div>
+		</frameElement>
+	);
 }
 
 export default App;
